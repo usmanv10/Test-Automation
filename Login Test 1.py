f8754a360345dc7c3833f174c3748f7707478e2a
usmanv10 <91057651+usmanv10@users.noreply.github.com>
2022-10-15 15:39:31 +0100

    Update Login Test 1.py

--- Login Test 1.py
+++ Login Test 1.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.remote.webelement import WebElement
 
 # Using a service to open the chrome driver. Needs to be defined above
 s=Service("C:/Users/usman.ali/PycharmProjects/pythonProject/chromedriver.exe")
+
+# define the values
+username = "espe.admin@collectionhq.com"
+password = "d1sc0ver"
 
 # Starts the chrome driver
 driver = webdriver.Chrome()
@@ -15,24 +18,33 @@ driver.maximize_window()
 url = "https://web.esp-test.net/enterprise/ets/login"
 driver.get(url)
 
-username = "espe.admin@collectionhq.com"
-password = "d1sc0ver"
 
 driver.implicitly_wait(10)
+
+# finds the username textbox and enters the username
 search = driver.find_element(By.ID, "username")
 search.send_keys("espe.admin@collectionhq.com")
 
+# finds the password textbox and enters the password
 search = driver.find_element(By.ID, "password")
 search.send_keys("d1sc0ver")
 
+# finds the sign in button and clicks it
 search = driver.find_element(By.ID, "submit_signin").click()
+
+print("logged in successfully")
 
 driver.implicitly_wait(10)
 
-search = driver.find_element(By.XPATH("//*[text()='Carts']"));
+# Tries to find the text "Carts"
 
-# search = driver.find_element(By.NAME, "Carts")
+driver.find_element(By.XPATH, "//*[contains(text(), 'Carts')]")
 
-# search.send_keys(Keys.ENTER)
+print ("the text Carts has been found!")
+
+driver.close()
+
+
+
 
 
