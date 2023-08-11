23ff8171e21fe84027393497b1d009fb8e8d4763
usmanv10 <91057651+usmanv10@users.noreply.github.com>
2023-08-11 14:34:11 +0100

    Add files via upload

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -5,7 +5,6 @@ class LoginPage:
     # Login Page
     textbox_username_id = "Email"
     textbox_password_id = "Password"
-    button_login_xpath = "/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/button"
     button_css_selector = "button[type='submit']"
     link_logout_linktext = "Logout"
 
@@ -21,7 +20,6 @@ class LoginPage:
         self.driver.find_element(By.ID, self.textbox_password_id).send_keys(password)
 
     def clickLogin(self):
-       # self.driver.find_element(By.XPATH, self.button_login_xpath).click()
         self.driver.find_element(By.CSS_SELECTOR, self.button_css_selector).click()
 
     def clickLogout(self):
